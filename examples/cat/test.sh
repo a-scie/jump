df676064f92de83a30a5472b3766f81cac1b1a76
John Sirois <john.sirois@gmail.com>
2022-11-12 09:20:49 -0800

    Dynamically generate the scie.jump object.

--- examples/cat/test.sh
+++ examples/cat/test.sh
@@ -16,6 +16,9 @@ gc "${PWD}/${JAVA}.sha256"
 
 ./"${JAVA}" "scie-jump boot-pack"
 
+SCIE_JUMP_SIZE="$(SCIE="inspect" ./"${JAVA}" | jq -r '.scie.jump.size')"
+SCIE_JUMP_VERSION="$(SCIE="inspect" ./"${JAVA}" | jq -r '.scie.jump.version')"
+
 # We should be able to assemble an identical scie using cat.
 JDK="$(jq -r '.scie.lift.files[0].name' "${LIFT}")"
 SCIE_CAT="java.cat${EXE_EXT}"
@@ -27,11 +30,18 @@ function scie_cat() {
     "${SCIE_JUMP}" \
     "${JDK}" \
     cowsay-1.1.0.jar \
-    <(echo) <(jq -c "${expression}" "${LIFT}") > "${SCIE_CAT}"
+    <(echo) \
+    <(
+      jq -c "
+      setpath([\"scie\", \"jump\", \"size\"]; ${SCIE_JUMP_SIZE})
+      | setpath([\"scie\", \"jump\", \"version\"]; \"${SCIE_JUMP_VERSION}\")
+      | ${expression}
+      " "${LIFT}"
+    ) > "${SCIE_CAT}"
   chmod +x "${SCIE_CAT}"
 }
 
-scie_cat '.'
+scie_cat "."
 
 sha256 "${SCIE_CAT}" > "${SCIE_CAT}.sha256"
 gc "${PWD}/${SCIE_CAT}.sha256"
@@ -45,17 +55,17 @@ else
   die "Hashes of ${JAVA} and ${SCIE_CAT} were not the same."
 fi
 
-scie_cat 'del(.scie.jump)'
+scie_cat "del(.scie.jump)"
 {
   ./"${SCIE_CAT}" "missing jump" && die "Expected a missing scie.jump to cause a boot error."
 } || log "^- Expected boot error observed"
 
-scie_cat 'del(.scie.lift.files[0].type)'
+scie_cat "del(.scie.lift.files[0].type)"
 {
   ./"${SCIE_CAT}" "missing jump" && die "Expected a missing file type to cause a boot error."
 } || log "^- Expected boot error observed"
 
-scie_cat 'del(.scie.lift.files[0].hash)'
+scie_cat "del(.scie.lift.files[0].hash)"
 {
   ./"${SCIE_CAT}" "missing jump" && die "Expected a missing file hash to cause a boot error."
 } || log "^- Expected boot error observed"
