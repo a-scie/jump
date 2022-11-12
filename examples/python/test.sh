f541cb824ff40d5d5b1045c35accf8432e1a5c76
John Sirois <john.sirois@gmail.com>
2022-11-12 12:01:26 -0800

    Fix blob locks.
    Previously the blob parent directory was locked instead of the lob
    itself and this could lead to the blob not being re-unpacked if it
    was deleted after 1st unpack.
    
    A previously failing test of this is added to the python example where
    the Pants binding command does, in fact, delete the pants.pex blob.

--- examples/python/test.sh
+++ examples/python/test.sh
@@ -8,8 +8,19 @@ trap gc EXIT
 "${SCIE_JUMP}" "${LIFT}"
 gc "${PWD}/pants" "${PWD}/.pants.d" "${PWD}/.pids"
 
-time RUST_LOG=trace ./pants --no-pantsd -V
-time RUST_LOG=debug ./pants --no-pantsd -V
+# Observe initial install and subsequent short-circuiting of install activity.
+time RUST_LOG=trace ./pants -V
+time RUST_LOG=debug ./pants -V
 
 # Use the built-in BusyBox functionality via env var.
 SCIE_BOOT=repl ./pants -c 'from pants.util import strutil; print(strutil.__file__)'
+
+# Confirm boot bindings re-run successfully when the lift manifest changes - which allocates a new
+# boot bindings directory.
+jq '
+setpath(["extra"]; 42)
+| setpath(["scie", "lift", "name"]; "pants-extra")
+' "${LIFT}" > lift.json
+gc "${PWD}/lift.json" "${PWD}/pants-extra"
+"${SCIE_JUMP}"
+time RUST_LOG=debug ./pants-extra -V
