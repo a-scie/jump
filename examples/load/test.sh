73f6ecc2d952f075f2fba2ed20b1e6ad622c029b
John Sirois <john.sirois@gmail.com>
2025-03-10 13:05:22 -0700

    Fix test under Windows for real.

--- examples/load/test.sh
+++ examples/load/test.sh
@@ -10,9 +10,14 @@ check_cmd mktemp
 gc "${PWD}/cowsay"
 "${SCIE_JUMP}" "${LIFT}"
 
+# N.B.: The file size differences here are due to CRLF line endings on git checkout under Windows.
 split_dry_run_output="$(SCIE="split" ./cowsay -n -- get.sh)"
-if [ "get.sh 319 executable" != "${split_dry_run_output}" ]; then
-  die "Expected a split dry run to indicate get.sh is executable. Got:\n${split_dry_run_output}"
+if [ "${OS}" == "windows" ] && [ "get.sh 332 blob" != "${split_dry_run_output}" ]; then
+  die "
+Expected a split dry run to indicate get.sh is a blob on Windows. Got:\n${split_dry_run_output}"
+elif [ "get.sh 319 executable" != "${split_dry_run_output}" ]; then
+  die "
+Expected a split dry run to indicate get.sh is executable on Unix. Got:\n${split_dry_run_output}"
 fi
 gc "${PWD}/split"
 SCIE="split" ./cowsay split -- get.sh
