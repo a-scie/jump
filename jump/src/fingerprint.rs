c09d663a37bc12ff36d215824b008ef0a2cd4d1a
John Sirois <john.sirois@gmail.com>
2026-04-02 11:08:42 -0700

    Upgrade sha2 and other deps. (#397)
    Supercedes #393.

--- jump/src/fingerprint.rs
+++ jump/src/fingerprint.rs
@@ -1,7 +1,8 @@
 // Copyright 2022 Science project contributors.
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-use std::io::Read;
+use std::io;
+use std::io::{BufRead, BufReader, Read};
 use std::path::Path;
 
 use logging_timer::time;
@@ -9,7 +10,7 @@ use sha2::{Digest, Sha256};
 
 #[time("debug", "fingerprint::{}")]
 pub fn digest(data: &[u8]) -> String {
-    format!("{digest:x}", digest = Sha256::digest(data))
+    hex::encode(Sha256::digest(data))
 }
 
 pub fn digest_file(path: &Path) -> Result<(u64, String), String> {
@@ -23,10 +24,33 @@ pub fn digest_file(path: &Path) -> Result<(u64, String), String> {
 }
 
 #[time("debug", "fingerprint::{}")]
-pub fn digest_reader<R: Read>(mut reader: R) -> Result<(u64, String), String> {
+pub fn digest_reader<R: Read>(reader: R) -> Result<(u64, String), String> {
     let mut hasher = Sha256::new();
-    let copied_size = std::io::copy(&mut reader, &mut hasher)
-        .map_err(|e| format!("Failed to digest stream: {e}"))?;
-    let hash = format!("{digest:x}", digest = hasher.finalize());
-    Ok((copied_size, hash))
+    let copied_size =
+        hash_reader(reader, &mut hasher).map_err(|e| format!("Failed to digest stream: {e}"))?;
+    let hash = hex::encode(hasher.finalize());
+    let size = u64::try_from(copied_size)
+        .map_err(|err| format!("File was bigger than a u64 at {copied_size} bytes!: {err}"))?;
+    Ok((size, hash))
+}
+
+pub fn hash_reader<R, D>(reader: R, digest: &mut D) -> Result<usize, io::Error>
+where
+    R: Read,
+    D: Digest,
+{
+    let mut size: usize = 0;
+    let mut reader = BufReader::new(reader);
+    loop {
+        let amount_read = {
+            let buf = reader.fill_buf()?;
+            if buf.is_empty() {
+                return Ok(size);
+            }
+            digest.update(buf);
+            buf.len()
+        };
+        size += amount_read;
+        reader.consume(amount_read);
+    }
 }
